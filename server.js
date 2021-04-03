7ecc74f777bdfab59df82322aab47404f5acd8d5
yetabalij <ermiastsegu@yahoo.com>
2021-04-03 20:28:44 +0300

    database connection is created

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 //importing required packages
+const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config();
+
+//database connection
+mongoose
+  .connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+  });
 
 const port = process.env.PORT || 5000;
 
