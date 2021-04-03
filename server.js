283e6edd5831c528b3887953a87729ecac539dcf
yetabalij <ermiastsegu@yahoo.com>
2021-04-03 19:27:58 +0300

    config directory is added and file path is changed.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //importing required packages
 const app = require("./app");
-const config = require("./config");
+const config = require("./config/config");
 
 app.listen(config.PORT, () => {
   console.log(`application started to: https://localhost:${config.PORT}`);
