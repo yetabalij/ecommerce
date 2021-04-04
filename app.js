b0232a4d0deb9953f38cb650b40cc015138ad7dd
yetabalij <ermiastsegu@yahoo.com>
2021-04-04 20:56:39 +0300

    app.js is modifed.

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // importing required packages
 const express = require("express");
 const morgan = require("morgan");
-const bodyparser = require("body-parser");
 const userRoute = require("./routes/user");
 
 const app = express();
 
 //middlewares
 app.use(morgan("dev"));
-app.use(bodyparser.json());
+app.use(express.json());
 
 //router middlewares
 app.use("/api", userRoute);
