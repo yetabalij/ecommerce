602c8170162a3708a8bc724d22394e983f1da797
yetabalij <ermiastsegu@yahoo.com>
2021-04-04 20:44:55 +0300

    bodyparser package is added.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // importing required packages
 const express = require("express");
 const morgan = require("morgan");
+const bodyparser = require("body-parser");
 const userRoute = require("./routes/user");
 
 const app = express();
 
 //middlewares
 app.use(morgan("dev"));
+app.use(bodyparser.json());
 
 //router middlewares
 app.use("/api", userRoute);
